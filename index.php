6c94f016a5d6de7e485294e834d9b5819b97f7d1
vregis <vregis@mail.ru>
2016-10-07 14:51:05 +0300

    refactor index

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 include 'autoload.php';
 
-$run = new \controller\Main(substr($_SERVER['REQUEST_URI'], 1));
+$run = new \controller\Main(substr($_SERVER['REQUEST_URI'], 1)==''?'run':substr($_SERVER['REQUEST_URI'], 1));
